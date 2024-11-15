5809a93100af80eba6495732f619d3f66d058c84
Balinhui <balinhui2023@gmail.com>
2024-11-15 23:19:00 +0800

    Site updated: 2024-11-15 23:18:59

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1731677310880";
+const VERSION = "1731683817272";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
