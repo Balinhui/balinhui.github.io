e4442be39bb6ab9ba54dc6d1a1398eb122c095f8
Balinhui <balinhui2023@gmail.com>
2024-12-08 21:51:27 +0800

    Site updated: 2024-12-08 21:51:27

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1733663215374";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1733665882829";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
