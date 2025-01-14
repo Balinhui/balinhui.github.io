9cc4d68534df79f1ebdec0a292bdb2272aca27f9
Balinhui <balinhui2023@gamil.com>
2025-01-14 22:36:05 +0800

    Site updated: 2025-01-14 22:36:05

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1733838438860";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1736865358854";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
